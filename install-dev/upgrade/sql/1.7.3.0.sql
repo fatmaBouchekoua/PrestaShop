cc868069610a9d458fa37f94b05b33976c8928f2
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2017-06-19 14:40:12 +0100

    BO: Translate stores schedules, name and adress from BO

--- install-dev/upgrade/sql/1.7.3.0.sql
+++ install-dev/upgrade/sql/1.7.3.0.sql
@@ -3,3 +3,16 @@ SET NAMES 'utf8';
 
 UPDATE `PREFIX_tab` SET `position` = 0 WHERE `class_name` = 'AdminZones' AND `position` = '1';
 UPDATE `PREFIX_tab` SET `position` = 1 WHERE `class_name` = 'AdminCountries' AND `position` = '0';
+
+CREATE TABLE IF NOT EXISTS `PREFIX_store_lang` (
+  `id_store` int(11) unsigned NOT NULL,
+  `id_lang` int(11) unsigned NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `address1` varchar(128) NOT NULL,
+  `address2` varchar(128) DEFAULT NULL,
+  `hours` text,
+  `note` text,
+  PRIMARY KEY (`id_store`, `id_lang`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
+
+/* PHP:migrate_data_from_store_to_store_lang_and_clean_store(); */;
