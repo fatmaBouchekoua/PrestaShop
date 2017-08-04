46901cb9fafb5fe48d684cdc8e2ff5b9fd9fcc17
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2017-08-04 17:27:26 +0100

    IN: Add store translations to fixtures

--- install-dev/upgrade/sql/1.7.3.0.sql
+++ install-dev/upgrade/sql/1.7.3.0.sql
@@ -7,9 +7,9 @@ UPDATE `PREFIX_tab` SET `position` = 1 WHERE `class_name` = 'AdminCountries' AND
 CREATE TABLE IF NOT EXISTS `PREFIX_store_lang` (
   `id_store` int(11) unsigned NOT NULL,
   `id_lang` int(11) unsigned NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `address1` varchar(128) NOT NULL,
-  `address2` varchar(128) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `address1` varchar(255) NOT NULL,
+  `address2` varchar(255) DEFAULT NULL,
   `hours` text,
   `note` text,
   PRIMARY KEY (`id_store`, `id_lang`)
