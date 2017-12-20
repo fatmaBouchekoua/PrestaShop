f6484740bef8b51016835efca24709264f6ef625
fatmaBouchekoua <fatma.bouchekoi@debian>
2017-12-20 18:18:34 +0100

    Add create basic product

--- tests/HighTests/test/selectors/FO/product_page.js
+++ tests/HighTests/test/selectors/FO/product_page.js
@@ -4,6 +4,9 @@ module.exports = {
     first_product_size: '//*[@id="group_1"]',
     first_product_quantity: '//*[@id="quantity_wanted"]',
     first_product_color: '//*[@id="group_3"]/li[2]/label/input',
+    product_name_label: '//*[@id="main"]//h1[@itemprop="name"]',
+    product_price_label: '//*[@id="main"]//div[@class="current-price"]/span[@itemprop="price"]',
+    
 
   }
 };
