75fe0cacc50e8277140ef58482a7646f4c2d55d4
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2018-03-02 10:00:45 +0100

    TE: Add module name

--- tests/E2E/test/campaigns/regular/02_install_module.js
+++ tests/E2E/test/campaigns/regular/02_install_module.js
@@ -17,19 +17,19 @@ scenario('Install and Uninstall Module from cross selling', () => {
         .then(() => client.closeBoarding(OnBoarding.popup_close_button))
     });
   }, 'order');
-  scenario('Install "'+module_tech_name+'" From Cross selling', client => {
+  scenario('Install "' + module_tech_name + '" From Cross selling', client => {
     module_common_scenarios.installModule(client, ModulePage, AddProductPage, module_tech_name);
   }, 'common_client');
-  scenario('Check Configuration page of "'+module_tech_name+'"', client => {
+  scenario('Check Configuration page of "' + module_tech_name + '"', client => {
     module_common_scenarios.checkConfigPage(client, ModulePage, module_tech_name);
   }, 'common_client');
-  scenario('Disable Module "'+module_tech_name+'"', client => {
+  scenario('Disable Module "' + module_tech_name + '"', client => {
     module_common_scenarios.disableModule(client, ModulePage, AddProductPage, module_tech_name);
   }, 'common_client');
-  scenario('Disable Module "'+module_tech_name+'"', client => {
+  scenario('Disable Module "' + module_tech_name + '"', client => {
     module_common_scenarios.enableModule(client, ModulePage, AddProductPage, module_tech_name);
   }, 'common_client');
-  scenario('Uninstall "'+module_tech_name+'"', client => {
+  scenario('Uninstall "' + module_tech_name + '"', client => {
     module_common_scenarios.uninstallModule(client, ModulePage, AddProductPage, module_tech_name);
   }, 'common_client');
 }, 'common_client', true);
